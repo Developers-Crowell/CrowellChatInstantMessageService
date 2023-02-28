bf23461d33db0ad27ef308f2a255f3108d424204
Cahlien <78377986+Cahlien@users.noreply.github.com>
2023-02-28 14:26:21 -0600

    add test for context loading

---
+++ src/test/java/dev/crowell/instantmessageservice/InstantMessageServiceApplicationTests.java
@@ -0,0 +1,31 @@
+package dev.crowell.instantmessageservice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.crowell.instantmessageservice.handlers.InstantMessageEventHandler;
+import dev.crowell.instantmessageservice.repos.InstantMessageRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class InstantMessageServiceApplicationTests {
+    @Autowired
+    private InstantMessageRepository repository;
+    @Autowired
+    private InstantMessageEventHandler handler;
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertAll(
+                () -> Assertions.assertNotNull(repository),
+                () -> Assertions.assertNotNull(handler),
+                () -> Assertions.assertNotNull(mapper)
+        );
+    }
+
+}
