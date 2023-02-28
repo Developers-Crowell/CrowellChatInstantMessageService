edcd6f09eb3081ad25b1f7ce3fde916f14ee550b
Cahlien <78377986+Cahlien@users.noreply.github.com>
2023-02-27 19:39:06 -0600

    update application and remove tests

---
+++ src/main/java/dev/crowell/instantmessageservice/InstantMessageServiceApplication.java
@@ -0,0 +1,31 @@
+package dev.crowell.instantmessageservice;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InstantMessageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstantMessageServiceApplication.class, args);
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("CrowellChat Instant Message API")
+                        .description("The API for the CrowellChat instant message service.")
+                        .version("v0.1.0")
+                        .license(new License().name("MIT").url("https://mit-license.org/")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("CrowellChat Documentation")
+                        .url("https://crowell.dev/crowellchat"));
+    }
+
+
+
+}
